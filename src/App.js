eec5552c8e7e6cbdd3bff67b8a91d749df99556d
Nona <81672054+Naru-hub@users.noreply.github.com>
2022-02-23 21:37:37 +0900

    radio-button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
+import { useState } from "react";
 import "./styles.css";
 
-export default function App() {
+const InputRadio = () => {
+  const [checkedValue, setCheckedValue] = useState("赤");
+  const handleChange = (e) => setCheckedValue(e.target.value);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <p>
+        現在選択されている値 : <b>{checkedValue}</b>
+      </p>
+      <label>
+        <input
+          type="radio"
+          value="赤"
+          onChange={handleChange}
+          checked={checkedValue === "赤"}
+        />
+        赤
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="青"
+          onChange={handleChange}
+          checked={checkedValue === "青"}
+        />
+        青
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="黄"
+          onChange={handleChange}
+          checked={checkedValue === "黄"}
+        />
+        黄
+      </label>
     </div>
   );
+};
+
+export default function App() {
+  return <InputRadio />;
 }
